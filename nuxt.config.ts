661179f8bcfe561732e8942fbd0eb709529920af
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-12 20:54:24 +0200

    :tada: Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+export default defineNuxtConfig({
+  ssr: true,
+  vite: false,
+  srcDir: 'src',
+})
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  Test {{ count }}
+  <button @click.prevent="add()">Ajouter 1</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+
+function add () {
+  count.value++
+}
+
+const router = useRouter()
+console.log(router.getRoutes().map((route) => route.path))
+</script>
